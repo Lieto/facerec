8c1fa1ea3dccec4fa296d439f802562eb8a125df
root <root@ip-172-31-20-169.eu-west-1.compute.internal>
2015-10-18 09:10:47 +0000

    Bug fixes: changed route

--- facerec.py
+++ facerec.py
@@ -2,18 +2,18 @@ import os
 from flask import Flask, request, flash
 from flask.helpers import send_file
 
-DEBUG=True
-UPLOAD_FOLDER = '/var/www/facerec/data'
-PROCESSED_FOLDER = '/var/www/facerec/data'
+#DEBUG=True
+#UPLOAD_FOLDER = '/var/www/facerec/data'
+#PROCESSED_FOLDER = '/var/www/facerec/data'
 
 ALLOWED_EXTENSIONS = set(['jpg'])
 
 facerec = Flask(__name__)
 facerec.config.from_object(__name__)
 
-@app.route('/')
+@facerec.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Hello World, Facerec application talking'
 
 if __name__ == '__main__':
     facerec.run()
