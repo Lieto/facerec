d34bfa36a5276058484223314c59e9750a58b1ea
Lieto <vkuoppala@hotmail.com>
2015-10-29 10:14:05 +0200

    Added index.html, api calls to load image from url and display images

--- facerec.py
+++ facerec.py
@@ -3,27 +3,46 @@ import requests
 from flask import Flask, request, redirect, flash, url_for
 from flask.helpers import send_file, send_from_directory
 from image import Image
+from naivedlib import NaiveDlib
 from flask.templating import render_template
 
-DEBUG=True
-SECRET_KEY = 'development key'
-UPLOAD_FOLDER = '/var/www/facerec/data'
+#DEBUG=True
+#SECRET_KEY = 'development key'
+#UPLOAD_FOLDER = '/var/www/facerec/data'
 #UPLOAD_FOLDER = './data'
 
-PROCESSED_FOLDER = '/var/www/facerec/data'
+#PROCESSED_FOLDER = '/var/www/facerec/data'
 
-ALLOWED_EXTENSIONS = set(['jpg'])
+#ALLOWED_EXTENSIONS = set(['jpg'])
+#FACE_MEANS_FILE = 'mean.csv.txt'
+#LANDMARKS_FILE = 'shape_predictor_68_face_landmarks.dat'
+
 
 facerec = Flask(__name__)
-facerec.config.from_object(__name__)
+#facerec.config.from_pyfile('./config.cfg', silent=True)
+#Configuration
+
+facerec.config['DEBUG'] = True
+facerec.config['SECRET_KEY'] = 'development_key'
+facerec.config['UPLOAD_FOLDER'] = './data'
+facerec.config['PROCESSED_FOLDER'] = './data'
+facerec.config['ALLOWED_EXTENSIONS'] = set(['jpg'])
+facerec.config['FACE_MEANS_FILE'] = 'mean.csv.txt'
+facerec.config['LANDMARKS_FILE'] = 'shape_predictor_68_face_landmarks.dat'
+
+detector = NaiveDlib(os.path.join(facerec.config['UPLOAD_FOLDER'], facerec.config['FACE_MEANS_FILE']), 
+                             os.path.join(facerec.config['UPLOAD_FOLDER'], facerec.config['LANDMARKS_FILE']), facerec)
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1] in facerec.config['ALLOWED_EXTENSIONS']
 
 
-@facerec.route('/')
+@facerec.route('/', methods = ['GET', 'POST'])
 def hello_world():
-    return 'Hello World, Facerec application talking'
+    entries = []
+    
+    return render_template('index.html', entries = entries)
+    #return 'Hello World, Facerec application talking'
 
 @facerec.route('/upload_from_url', methods = ['GET', 'POST'])
 def upload_from_url():
@@ -49,7 +68,30 @@ def upload_from_url():
       <input type=submit value=Upload>
     </form>
     '''
+@facerec.route('/detect_face_from_url', methods = ['GET', 'POST'])
+def detect_face_from_url():
+    if request.method == 'POST':
+        url = request.form['url']
+        filename = url.split('/')[-1]
+        image_file = requests.get(url)
         
+        if image_file and allowed_file(filename):
+            with open(os.path.join(facerec.config['UPLOAD_FOLDER'], filename), 'wb') as f:
+                f.write(image_file.content)
+                
+                f.close()
+                
+                image = Image('jpg', filename.split(".")[0], os.path.join(facerec.config['UPLOAD_FOLDER'], filename), facerec.config)
+                image.getRGB(cache=True)
+                image.detect_face()
+        
+                face_imagename = filename.split('.')[0] + "_face" + ".jpg"
+                entries = []
+                entries.append(filename)
+                entries.append(face_imagename)
+                return render_template('index.html', entries = entries)
+
+                        
 @facerec.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -58,6 +100,9 @@ def upload_file():
             image_file.save(os.path.join(facerec.config['UPLOAD_FOLDER'], image_file.filename))
             flash('Uploaded file')
             
+            #detect_face()
+
+"""
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -68,6 +113,8 @@ def upload_file():
         <input type=submit value=Upload>
     </form>
     '''
+"""
+
 
 @facerec.route('/get_image')
 def get_image():
@@ -77,35 +124,33 @@ def get_image():
     
 @facerec.route('/uploads/<filename>')
 def send_file(filename):
+    #return send_from_directory('.', filename)
     return send_from_directory(facerec.config['UPLOAD_FOLDER'], filename)
 
+
     
-@facerec.route('/detect_face', methods=['GET', 'POST'])
-def detect_face():
+@facerec.route('/detect_face_from_file', methods=['GET', 'POST'])
+def detect_face_from_file():
     if request.method == 'POST':
         image_file = request.files['file']
         if image_file and allowed_file(image_file.filename):
             image_file.save(os.path.join(facerec.config['UPLOAD_FOLDER'], image_file.filename))
-            image = Image('jpg', image_file.filename.split(".")[0], os.path.join(facerec.config['UPLOAD_FOLDER'], image_file.filename))
+            image = Image('jpg', image_file.filename.split(".")[0], os.path.join(facerec.config['UPLOAD_FOLDER'], image_file.filename), facerec.config)
             image.getRGB(cache=True)
-            image.detect_face()
+            
+            largestBox = detector.getLargestFaceBoundingBox(image.rgb)
+            alignedFace = detector.alignImg("homography", 256, image.rgb, largestBox, outputPrefix=image.name, outputDebug=True, expandBox=False)
+            
+            #image.detect_face()
         
-            face_imagename = image_file.filename.split('.')[0] + "_face" + ".jpg"
+            orig_filename = image_file.filename.split('.')[0] + "-orig" + ".jpg"
+            face_imagename = image_file.filename.split('.')[0] + "-annotated" + ".jpg"
             entries = []
-            entries.append(image_file.filename)
+            entries.append(orig_filename)
             entries.append(face_imagename)
-            return render_template('show_images.html', entries = entries)
-            #return redirect(url_for('show_images'))
-            #return send_file(os.path.join(facerec.config['UPLOAD_FOLDER'], face_imagename), mimetype = 'image/jpg')
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-        <p><input type=file name=file>
-        <input type=submit value=Upload>
-    </form>
-    '''
+            entries.append(image_file.filename)
+            return render_template('index.html', entries = entries)
+
 
 
 
--- image.py
+++ image.py
@@ -3,16 +3,19 @@ Created on 21.10.2015
 
 @author: Vesa
 '''
+
+import os
 from skimage import io
 from naivedlib import NaiveDlib, transformPoints, annotate
 
 class Image:
     
-    def __init__(self, cls, name, path):
+    def __init__(self, cls, name, path, config):
         self.cls = cls
         self.name = name
         self.path = path
         self.rgb = None
+        self.config = config
     
     def getRGB(self, cache=False):
         if self.rgb is not None:
@@ -37,16 +40,22 @@ class Image:
             io.imsave(filename, self.rgb)
     
     def detect_face(self):
-        detector = NaiveDlib('/var/www/facerec/data/mean.csv.txt', '/var/www/facerec/data/shape_predictor_68_face_landmarks.dat')
+        detector = NaiveDlib(os.path.join(self.config['UPLOAD_FOLDER'], self.config['FACE_MEANS_FILE']), 
+                             os.path.join(self.config['UPLOAD_FOLDER'], self.config['LANDMARKS_FILE']))
         largestBox = detector.getLargestFaceBoundingBox(self.rgb)
         
-        points = detector.align(self.rgb, largestBox)
-        meanAlignPoints = transformPoints(detector.normMeanAlignPoints, largestBox, True)
+        if largestBox != None:
+            points = detector.align(self.rgb, largestBox)
+            meanAlignPoints = transformPoints(detector.normMeanAlignPoints, largestBox, True)
         
-        rectImage = annotate(self.rgb, largestBox, points = points, meanPoints = meanAlignPoints)
+            rectImage = annotate(self.rgb, largestBox, points = points, meanPoints = meanAlignPoints)
         
-        filename = "/var/www/facerec/data/" + self.name + "_face.jpg"
-        io.imsave(filename, rectImage)
+            filename = self.name + "_face.jpg"
+            filename = os.path.join(self.config['UPLOAD_FOLDER'], filename)
+            #filename = "./data/" + self.name + "_face.jpg"
+            io.imsave(filename, rectImage)
+            
+        
     
         
     
